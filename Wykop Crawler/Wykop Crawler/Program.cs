d76aa7f06d7cc28df69e1de3b9a5e55a5c93e451
Paweł Sobczak <pawel.sobczak0@gmail.com>
2018-12-23 12:35:49 +0100

    IDK

---
+++ Wykop Crawler/Wykop Crawler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wykop_Crawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
